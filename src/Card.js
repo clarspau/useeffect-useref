6edd18154ea179e5e098003d946e5286892fcc8f
clarspau <125636195+clarspau@users.noreply.github.com>
2024-03-09 18:04:53 -0800

    created a new component called Card.js and its corresponding css file Card.css

---
+++ src/Card.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+
+/** Card: renders a single card. */
+
+function Card({ name, image }) {
+     // get these once; it will never be updated for the same card
+
+     const [{ angle, x, y }] = useState({
+          angle: Math.random() * 90 - 45,
+          x: Math.random() * 40 - 20,
+          y: Math.random() * 40 - 20
+     });
+
+     const transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+
+     return <img
+          className="Card"
+          alt={name}
+          src={image}
+          style={{ transform }}
+     />;
+}
+
+export default Card;
